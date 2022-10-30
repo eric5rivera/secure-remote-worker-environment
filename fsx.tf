5aa92bef4f5413eb044cc0fdeb92ea8ac930f473
Eric Rivera <me@ericmrivera.com>
2022-10-29 21:34:00 -1000

    Add EC2 instances for bastion server and RADIUS server

--- fsx.tf
+++ fsx.tf
@@ -1,13 +1,13 @@
 # FSx
 resource "aws_fsx_windows_file_system" "terraform-managed-fsx" {
   active_directory_id = aws_directory_service_directory.aws-managed-ad.id
-  deployment_type = "MULTI_AZ_1"
+  deployment_type     = "MULTI_AZ_1"
   kms_key_id          = aws_kms_key.fsx-kms.arn
   storage_capacity    = 32
   subnet_ids          = module.vpc.private_subnets
   preferred_subnet_id = element(module.vpc.private_subnets, 0)
   throughput_capacity = 32
-  security_group_ids = [aws_security_group.allow_smb.id]
+  security_group_ids  = [aws_security_group.allow_smb.id]
 
 }
 
@@ -17,10 +17,10 @@ resource "aws_security_group" "allow_smb" {
   vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -31,7 +31,7 @@ resource "aws_security_group" "allow_smb" {
 
 # KMS Key for FSx encryption
 resource "aws_kms_key" "fsx-kms" {
-  description = "fsx key"
+  description             = "fsx key"
   deletion_window_in_days = 7
 }
 
