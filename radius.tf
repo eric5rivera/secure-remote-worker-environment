6dacce4c3473f81579e716d31fbcde529a117c1a
Eric Rivera <me@ericmrivera.com>
2022-11-06 10:26:03 -1000

    Remove duplicate ec2 resource

--- radius.tf
+++ radius.tf
@@ -12,27 +12,7 @@
 #   }
 
 #   owners = ["099720109477"] # Canonical
-# }
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "radius-server"
-
-  ami                    = "ami-08e2d37b6a0129927"
-  instance_type          = "t2.micro"
-  key_name               = "erivera-key-pair"
-  monitoring             = true
-  vpc_security_group_ids = [aws_security_group.radius-server.id]
-  subnet_id              = element(module.vpc.private_subnets, 0)
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-  
-}
+# 
 
 # Security group
 resource "aws_security_group" "radius-server" {
