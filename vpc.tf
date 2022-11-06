4fc3e26d06dd5b677a126988061bfcc2cfb39930
Eric Rivera <me@ericmrivera.com>
2022-11-06 06:58:29 -1000

    Add public subnets for network firewall

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,13 @@
 # VPC
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
-  name                   = "demo-dev"
+  name                   = "secure-remote-worker-vpc"
   cidr                   = "10.10.0.0/16"
   azs                    = ["us-west-2a", "us-west-2b"]
   private_subnets        = ["10.10.1.0/24", "10.10.2.0/24"]
-  public_subnets         = ["10.10.3.0/24", "10.10.4.0/24"]
+  private_subnet_suffix  = "private"
+  public_subnets         = ["10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+  public_subnet_suffix   = "public"
   enable_nat_gateway     = true
   single_nat_gateway     = true
   one_nat_gateway_per_az = false
